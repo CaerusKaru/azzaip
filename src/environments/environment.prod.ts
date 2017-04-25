fa0d994856085fc4b61fba9f87b6deaa03bbf9be
forsooth <maastntes@gmail.com>
2017-04-25 07:33:18 -0400

    Get production mode working

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_ENDPOINT: '52.204.216.32',
-  CC_ENDPOINT: '34.193.86.61'
+  API_ENDPOINT: 'http://52.204.216.32',
+  CC_ENDPOINT: 'http://34.193.86.61'
 };
